d8e14f9a43efc2afa7134ec829012c98f8c1ac6b
gilreich1 <gilreich12@gmail.com>
2023-07-27 13:22:22 +0300

    improvements on Switch util component

--- components/form/Switch.tsx
+++ components/form/Switch.tsx
@@ -18,9 +18,12 @@ interface SwitchProps {
   options: Option[];
   onPress: (v: string) => void;
   fontSize?: number;
+  /**
+   * Color of text of unselected item/s
+   */
   textColor?: AppColor;
   /**
-   * Color of text of selected item
+   * Color of text of selected item/s
    */
   selectedColor?: AppColor;
   /**
@@ -31,12 +34,28 @@ interface SwitchProps {
    * Item selected in initial render (default -1)
    */
   initial?: number;
-  /*
-   *Color of the item selected
+  /**
+   *Color of the item/s selected
    */
   buttonColor?: AppColor;
+  /*
+   *Color of the item/s selected
+   */
+  buttonMargin?: number;
+  /**
+   *Border Radius of the component
+   */
+  borderRadius?: number;
+  disabled?: boolean;
 }
 
+/**
+ * a wrapper for SwitchSelector from 'react-native-switch-selector' to provide type local support
+ */
 export default function Switch(props: SwitchProps) {
-  return <SwitchSelector {...props}></SwitchSelector>;
+  return props.disabled ? (
+    <SwitchSelector style={{ opacity: '40%' }} {...props}></SwitchSelector>
+  ) : (
+    <SwitchSelector {...props}></SwitchSelector>
+  );
 }
