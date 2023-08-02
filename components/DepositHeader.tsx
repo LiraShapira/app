e3c5da569a02b0ef41fd0933aa996bbb94d04bb8
gilreich1 <gilreich12@gmail.com>
2023-08-02 17:49:31 +0300

    placeholder deposit header

---
+++ components/DepositHeader.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+
+export default function DepositHeader() {
+  return (
+    <View
+      style={{
+        height: 40,
+        backgroundColor: 'white',
+      }}
+    >
+      <Text>Deposit Header</Text>
+    </View>
+  );
+}
