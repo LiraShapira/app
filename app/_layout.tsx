4419175f82f4d6f5da308cbbec977297f0938494
gilreich1 <gilreich12@gmail.com>
2023-09-28 16:29:58 +0300

    build config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -77,7 +77,7 @@ function RootLayoutNav() {
           dispatch(setUser(user));
         }
       });
-  });
+  }, []);
 
   return (
     <>
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Redirect } from 'expo-router';
+import 'react-native-gesture-handler';
 
 const Index = () => {
   return <Redirect href='/Home' />;
