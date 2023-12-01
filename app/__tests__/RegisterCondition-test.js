cd68968b77845772035f4c7e083857c0063bdfcd
Yaron-Jack <jackamichai@gmail.com>
2023-12-01 10:42:35 +0200

    test registration-start

---
+++ app/__tests__/RegisterCondition-test.js
@@ -0,0 +1,8 @@
+import "react-native";
+import React from "react";
+import { render } from "@testing-library/react-native";
+import Auth from "./Auth";
+
+test("renders Auth component", () => {
+  render(<Auth />);
+});
