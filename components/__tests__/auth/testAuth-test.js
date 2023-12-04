e77ef1f60082c0155e1c113076a9f0290370c850
Yaron-Jack <jackamichai@gmail.com>
2023-12-04 09:56:47 +0200

    file test reg

---
+++ components/__tests__/auth/testAuth-test.js
@@ -0,0 +1,58 @@
+import "react-native";
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
+import Auth from "./Auth";
+import { act } from "react-test-renderer";
+
+test("renders Auth component", () => {
+  render(<Auth />);
+});
+
+test('checking registration adds people properly. case 1', async () => {
+  // Given
+  const user = {
+    firstName: "Bob",
+    lastName: "Builder",
+    phoneNumber: "0951555555",
+    compostStand: "harakevet"
+  };
+
+  // Mock AsyncStorage
+  jest.mock('@react-native-async-storage/async-storage', () => ({
+    getItem: jest.fn(() => Promise.resolve(null)),
+    setItem: jest.fn(() => Promise.resolve()),
+  }));
+
+  // Mock router
+  const mockPush = jest.fn();
+  const mockRouter = {
+    push: mockPush,
+  };
+  jest.mock('expo-router', () => ({
+    useRouter: () => mockRouter,
+  }));
+
+  // Mock sendRegistrationForm function
+  const mockSendRegistrationForm = jest.fn(() => Promise.resolve({ data: user }));
+  jest.mock('../store/authFormSlice', () => ({
+    sendRegistrationForm: mockSendRegistrationForm,
+  }));
+
+  // When
+  const { getByPlaceholderText, getByText } = render(<Auth />);
+  
+  // Set input values
+  fireEvent.changeText(getByPlaceholderText('Enter phone number'), user.phoneNumber);
+
+  // Set compost stand (assuming there is only one option in the Picker)
+  fireEvent.changeText(getByText('harakevet'));
+
+  // Click on the register button
+  fireEvent.press(getByText('Register'));
+
+  // Then
+  await waitFor(() => {
+    expect(mockSendRegistrationForm).toHaveBeenCalled();
+    expect(mockRouter.push).toHaveBeenCalledWith('/Home');
+  });
+});
