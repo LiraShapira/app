62c74edac7f381c83b790c0c08d1f25b21f82cf4
Yaron-Jack <jackamichai@gmail.com>
2023-12-05 11:16:56 +0200

    small changes

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,17 @@
 module.exports = {
-    preset: "jest-expo",
-    transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
+  preset: "jest-expo",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  globals: {
+    "ts-jest": {
+      babelConfig: true,
+      tsconfig: "./tsconfig.json",
     },
-    globals: {
-      "ts-jest": {
-        transformerConfig: {
-          transformIgnorePatterns: [
-            "./node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)",
-            "jest-runner",
-          ],
-        },
-      },
-    },
-    testPathIgnorePatterns: [
-      "./node_modules/",
-      "@react-native",
-    ],
-    testEnvironment: "react-native",
-  };
+  },
+  testPathIgnorePatterns: [
+    "./node_modules/",
+    "@react-native",
+  ],
+  testEnvironment: "node",
+};
