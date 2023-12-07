0caeffa8526dc2964ad097729e6aa9de0606977c
Gil Reich <gil.reich@acronis.com>
2023-12-07 13:45:16 +0200

    example tests

--- components/__tests__/auth/testAuth-test.js
+++ components/__tests__/auth/testAuth-test.js
@@ -1,58 +1,73 @@
-console.log(process.env)
 import "react-native";
-import React from "react";
-import { render, fireEvent, waitFor } from "@testing-library/react-native";
-import Auth from "/Users/Lenovo/app/app/Auth.tsx";
+import React from 'react';
+import LoadingPage from '../../utils/LoadingPage.tsx';
+import Registration from '../../auth/Registration.tsx';
+import { advancedRender } from '../../../tests'
 
+jest.mock('expo-localization', () => ({
+  getLocales: () => [{ languageCode: 'en' }]
+}));
+
+
+test('renders correctly', () => {
+  const tree = advancedRender(
+  <LoadingPage />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('renders correctly', () => {
+  const tree = advancedRender(<Registration />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
 // test("renders Auth component", () => {
 //   render(<Auth />);
 // });
-test('checking registration adds people properly. case 1', async () => {
-  // Given
-  const user = {
-    firstName: "Bob",
-    lastName: "Builder",
-    phoneNumber: "0951555555",
-    compostStand: "harakevet"
-  };
+// test('checking registration adds people properly. case 1', async () => {
+//   // Given
+//   const user = {
+//     firstName: "Bob",
+//     lastName: "Builder",
+//     phoneNumber: "0951555555",
+//     compostStand: "harakevet"
+//   };
 
-  // Mock AsyncStorage
-  jest.mock('@react-native-async-storage/async-storage', () => ({
-    getItem: jest.fn(() => Promise.resolve(null)),
-    setItem: jest.fn(() => Promise.resolve()),
-  }));
+//   // Mock AsyncStorage
+//   jest.mock('@react-native-async-storage/async-storage', () => ({
+//     getItem: jest.fn(() => Promise.resolve(null)),
+//     setItem: jest.fn(() => Promise.resolve()),
+//   }));
 
-  // Mock router
-  const mockPush = jest.fn();
-  const mockRouter = {
-    push: mockPush,
-  };
-  jest.mock('expo-router', () => ({
-    useRouter: () => mockRouter,
-  }));
+//   // Mock router
+//   const mockPush = jest.fn();
+//   const mockRouter = {
+//     push: mockPush,
+//   };
+//   jest.mock('expo-router', () => ({
+//     useRouter: () => mockRouter,
+//   }));
 
-  // Mock sendRegistrationForm function
-  const mockSendRegistrationForm = jest.fn(() => Promise.resolve({ data: user }));
-  jest.mock('/Users/Lenovo/app/store/authFormSlice.ts', () => ({
-    sendRegistrationForm: mockSendRegistrationForm,
-  }));
+//   // Mock sendRegistrationForm function
+//   const mockSendRegistrationForm = jest.fn(() => Promise.resolve({ data: user }));
+//   jest.mock('/Users/Lenovo/app/store/authFormSlice.ts', () => ({
+//     sendRegistrationForm: mockSendRegistrationForm,
+//   }));
 
-  // When
-  const { getByPlaceholderText, getByText } = render(<Auth />);
+//   // When
+//   const { getByPlaceholderText, getByText } = render(<Auth />);
   
-  // Set input values
-  fireEvent.changeText(getByPlaceholderText('Enter phone number'), user.phoneNumber);
+//   // Set input values
+//   fireEvent.changeText(getByPlaceholderText('Enter phone number'), user.phoneNumber);
 
-  // Set compost stand (assuming there is only one option in the Picker)
-  fireEvent.changeText(getByText('harakevet'));
+//   // Set compost stand (assuming there is only one option in the Picker)
+//   fireEvent.changeText(getByText('harakevet'));
 
-  // Click on the register button
-  fireEvent.press(getByText('Register'));
+//   // Click on the register button
+//   fireEvent.press(getByText('Register'));
 
-  // Then
-  await waitFor(() => {
-    expect(mockSendRegistrationForm).toHaveBeenCalled();
-    expect(mockRouter.push).toHaveBeenCalledWith('/Home');
-  });
-});
+//   // Then
+//   await waitFor(() => {
+//     expect(mockSendRegistrationForm).toHaveBeenCalled();
+//     expect(mockRouter.push).toHaveBeenCalledWith('/Home');
+//   });
+// });
