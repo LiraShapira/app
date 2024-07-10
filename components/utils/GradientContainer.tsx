bd275ec69708b7a5ca2e3974c2e82531db8fea00
gilreich1 <gilreich12@gmail.com>
2024-07-10 19:15:28 +0300

    gradientContainer dark moe

--- components/utils/GradientContainer.tsx
+++ components/utils/GradientContainer.tsx
@@ -1,34 +1,19 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { ViewProps } from 'react-native';
+import { useColorScheme, ViewProps } from 'react-native';
 
 interface GradientContainerProps {
   children: JSX.Element | JSX.Element[];
   styles?: ViewProps;
-  startColor?: string;
-  endColor?: string;
 }
 
-/**
- * GradientComponent displays a gradient background from startColor to endColor.
- *
- * @component
- * @example
- * // Display default green to pink gradient
- * <GradientComponent />
- *
- * // Display custom gradient with specified colors
- * <GradientComponent startColor="#FF5733" endColor="#33FF57" />
- *
- * @param {string} [props.startColor='#00FF00'] - The starting color of the gradient. Default is green #D6FADE.
- * @param {string} [props.endColor='#FF69B4'] - The ending color of the gradient. Default is pink #EBDCF7.
- *
- */
 export default function GradientContainer({
   children,
   styles,
-  startColor ='#D6FADE',
-  endColor = '#F2E4EB',
 }: GradientContainerProps) {
+  const colorScheme = useColorScheme() || 'light';
+  const startColor = colorScheme === 'light' ? '#D6FADE' : '#121E4A';
+  const endColor = colorScheme === 'light' ? '#F2E4EB' : '#00AA8B';
+
   return (
     <LinearGradient
       colors={[startColor, endColor]}
