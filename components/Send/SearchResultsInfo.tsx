3c6d981373f052329374ed9512d01a4d4a750769
udayfakie <92738263+udayfakie@users.noreply.github.com>
2024-08-19 19:12:25 +0300

    parsing phone number

--- components/Send/SearchResultsInfo.tsx
+++ components/Send/SearchResultsInfo.tsx
@@ -1,12 +1,12 @@
-import { View, Text, useColorScheme } from 'react-native';
-import i18n from '../../translationService';
-import CustomButton from '../utils/CustomButton';
-import { isPossiblePhoneNumber } from 'libphonenumber-js';
-import {useLocalSearchParams, useRouter} from 'expo-router';
-import { ContactTypes } from 'expo-contacts';
-import { setChosenContact } from '../../store/sendFormSlice';
-import { useAppDispatch } from '../../hooks';
-import Colors from '../../constants/Colors';
+import { View, Text, useColorScheme } from "react-native";
+import i18n from "../../translationService";
+import CustomButton from "../utils/CustomButton";
+import { isPossiblePhoneNumber, parsePhoneNumber } from "libphonenumber-js";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { ContactTypes } from "expo-contacts";
+import { setChosenContact } from "../../store/sendFormSlice";
+import { useAppDispatch } from "../../hooks";
+import Colors from "../../constants/Colors";
 
 interface SearchResultsInfoProps {
   debouncedFilterTerms: string;
@@ -25,41 +25,44 @@ export default function SearchResultsInfo({
 
   const onSendToPhoneNumber = () => {
     const fakeContact = {
-      id: '1',
+      id: "1",
       contactType: ContactTypes.Person,
-      name: 'fake contact',
+      name: "fake contact",
       phoneNumbers: [
         {
-          label: 'fake',
-          id: '1',
-          number: debouncedFilterTerms,
+          label: "fake",
+          id: "1",
+          number: parsePhoneNumber(debouncedFilterTerms, "IL").nationalNumber,
         },
       ],
     };
+    console.log(fakeContact);
     dispatch(setChosenContact(fakeContact));
     if (isRequest) {
-      router.push({ pathname: '/SendAmount', params: { isRequest: true }});
+      router.push({ pathname: "/SendAmount", params: { isRequest: true } });
     } else {
-      router.push('/SendAmount');
+      router.push("/SendAmount");
     }
   };
 
   return (
     <View>
       {noContacts ? (
-        <View style={{ alignContent: 'center' }}>
+        <View style={{ alignContent: "center" }}>
           <Text
             style={{
-              color: Colors[colorScheme ?? 'light'].text,
+              color: Colors[colorScheme ?? "light"].text,
             }}
           >
-            {i18n.t('send_search_no_results', { search: debouncedFilterTerms })}
+            {i18n.t("send_search_no_results", { search: debouncedFilterTerms })}
           </Text>
           {/* if search is done for valid number, allow user to send directly to this number */}
-          {isPossiblePhoneNumber(debouncedFilterTerms, 'IL') === true && (
+          {isPossiblePhoneNumber(debouncedFilterTerms, "IL") === true && (
             <View>
               <CustomButton
-                text={i18n.t('send_search_no_results_send_to_number', { number: debouncedFilterTerms })}
+                text={i18n.t("send_search_no_results_send_to_number", {
+                  number: debouncedFilterTerms,
+                })}
                 onPress={onSendToPhoneNumber}
               />
             </View>
@@ -68,10 +71,10 @@ export default function SearchResultsInfo({
       ) : (
         <Text
           style={{
-            color: Colors[colorScheme ?? 'light'].text,
+            color: Colors[colorScheme ?? "light"].text,
           }}
         >
-          {i18n.t('send_search_searching_for')} {debouncedFilterTerms}
+          {i18n.t("send_search_searching_for")} {debouncedFilterTerms}
         </Text>
       )}
     </View>
