01bc4b6b3c348f9d50e7fa116bb99e12b7d321f1
gilreich1 <gilreich12@gmail.com>
2024-11-30 13:59:43 +0200

    use checkbox package for mobile apk

--- components/DepositFormCheckbox.tsx
+++ components/DepositFormCheckbox.tsx
@@ -1,21 +1,21 @@
-import { Text, View, useColorScheme } from 'react-native';
+import { Text, TextInput, View, useColorScheme } from 'react-native';
 import Colors from '../constants/Colors';
 import i18n from '../translationService';
-import { ChangeEvent } from 'react';
 import { useAppDispatch } from '../hooks';
 import { setGuaranteedAccurate } from '../store/depositFormSlice';
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
 const DepositFormCheckBox = () => {
   const colorScheme = useColorScheme() ?? 'light';
   const dispatch = useAppDispatch();
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    dispatch(setGuaranteedAccurate(e.target.checked));
+  const onChange = (isChecked: boolean) => {
+    dispatch(setGuaranteedAccurate(isChecked));
   };
 
   return (
     <View style={{ flexDirection: 'row', gap: 8 }}>
-      <input onChange={onChange} type='checkbox'></input>
+      <BouncyCheckbox fillColor={Colors[colorScheme].highlight} onPress={onChange} />
       <Text
         style={{
           color: Colors[colorScheme].text,
