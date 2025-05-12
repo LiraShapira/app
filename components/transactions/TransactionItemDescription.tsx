ad018e48fd862eb0a4abf7f541d9ec710e816069
gilreich1 <gilreich12@gmail.com>
2025-05-12 18:07:31 +0300

    fix/system to show other user in transactionItem

--- components/transactions/TransactionItemDescription.tsx
+++ components/transactions/TransactionItemDescription.tsx
@@ -1,7 +1,10 @@
-import {StyleSheet, Text, useColorScheme, View} from "react-native";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
 import Colors from "../../constants/Colors";
 import i18n from '../../translationService';
-import {Transaction} from "../../types/Transaction";
+import { Transaction } from "../../types/Transaction";
+import { useAppSelector } from "../../hooks";
+import { selectUser } from "../../store/userSlice";
+import { User } from "../../types/User";
 
 interface TransactionItemDescriptionProps {
   transaction: Transaction;
@@ -10,26 +13,31 @@ interface TransactionItemDescriptionProps {
 }
 
 
-export default function TransactionItemDescription({transaction, income, isRequest}: TransactionItemDescriptionProps) {
+export default function TransactionItemDescription({ transaction, income, isRequest }: TransactionItemDescriptionProps) {
   const colorScheme = useColorScheme() ?? 'light';
   const isDeposit = transaction.reason === 'Deposit'
+  const isStandAdminPayment = transaction.reason === 'StandAdminPayment';
   const topTextPrefix = isRequest ? i18n.t('transaction_request_to') : income ? i18n.t('transactions_list_received_from') : i18n.t('transactions_list_sent_to');
-  const topText = topTextPrefix + transaction.users[0].firstName
+  const currentUser = useAppSelector<User>(selectUser);
+  const otherUser =
+    transaction.users.find(u => u.id !== currentUser.id) ||
+    transaction.users[0];
+  const topText = topTextPrefix + otherUser.firstName;
 
   return (
-    <View style={ {...styles.notesDisplay} }>
+    <View style={{ ...styles.notesDisplay }}>
       {
         isDeposit ?
-          <Text style={ {fontWeight: '600', color: Colors[colorScheme].text} }>{ i18n.t('deposit') }</Text>
+          <Text style={{ fontWeight: '600', color: Colors[colorScheme].text }}>{i18n.t('deposit')}</Text>
           :
-            <Text
-              style={ {
-                fontWeight: '600',
-                color: Colors[colorScheme].text
-              } }>{ topText }</Text>
+          <Text
+            style={{
+              fontWeight: '600',
+              color: Colors[colorScheme].text
+            }}>{topText}</Text>
       }
-      <Text style={ {color: Colors[colorScheme].text} }>
-        { isDeposit ? i18n.t('transactions_list_compost_deposit') : transaction.reason }
+      <Text style={{ color: Colors[colorScheme].text }}>
+        {isDeposit ? i18n.t('transactions_list_compost_deposit') : isStandAdminPayment ? i18n.t('transaction_item_description_stand_admin_payment') : transaction.reason}
       </Text>
     </View>
   )
